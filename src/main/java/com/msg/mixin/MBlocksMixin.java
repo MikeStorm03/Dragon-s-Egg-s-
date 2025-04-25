c9f4d7d180c74d75f5531ed42e8c53ab1aab0905
MikeStorm03 <quockhanh.09.02.03@gmail.com>
2025-04-26 04:57:46 +0700

    better compatible

--- src/main/java/com/msg/mixin/MBlocksMixin.java
+++ src/main/java/com/msg/mixin/MBlocksMixin.java
@@ -3,24 +3,33 @@ package com.msg.mixin;
 import java.util.function.Function;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import com.msg.block.NewDragonEgg;
 
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
-import net.minecraft.registry.RegistryKey;
-import net.minecraft.registry.RegistryKeys;
-import net.minecraft.util.Identifier;
 
 @Mixin(Blocks.class)
 public class MBlocksMixin {
 
-    @Overwrite
-    private static Block register(String id, Function<AbstractBlock.Settings, Block> factory, AbstractBlock.Settings settings) {
-        factory = (id == "dragon_egg") ? NewDragonEgg::new : factory;
-        return Blocks.register((RegistryKey<Block>)RegistryKey.of(RegistryKeys.BLOCK, Identifier.ofVanilla(id)), factory, settings);
+    // Getting the block id using @Inject yet somehow it worked perfectly
+    private static String block_id;
+    @Inject(method = "Lnet/minecraft/block/Blocks;register(Ljava/lang/String;Ljava/util/function/Function;Lnet/minecraft/block/AbstractBlock$Settings;)Lnet/minecraft/block/Block;",
+            at = @At("HEAD"),
+            cancellable = false)
+    private static void getBlockID(String id, Function<AbstractBlock.Settings, Block> factory, AbstractBlock.Settings settings, CallbackInfoReturnable ci){
+        block_id = id;
     }
 
+    // Replace with the old dragon egg
+    @ModifyVariable(method = "Lnet/minecraft/block/Blocks;register(Ljava/lang/String;Ljava/util/function/Function;Lnet/minecraft/block/AbstractBlock$Settings;)Lnet/minecraft/block/Block;",
+                    at = @At("HEAD"))
+    private static Function<AbstractBlock.Settings, Block> injected(Function<AbstractBlock.Settings, Block> factory) {
+        return (block_id == "dragon_egg") ? NewDragonEgg::new : factory; 
+    }
 }
