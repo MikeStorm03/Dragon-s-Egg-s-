82b9abfc9e81e7b57f2f04a58be534ee6ffc0a89
MikeStorm03 <quockhanh.09.02.03@gmail.com>
2025-04-26 08:23:18 +0700

    for 1.21

--- src/main/java/com/msg/mixin/MBlocksMixin.java
+++ src/main/java/com/msg/mixin/MBlocksMixin.java
@@ -1,35 +1,31 @@
 package com.msg.mixin;
 
-import java.util.function.Function;
-
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import com.msg.block.NewDragonEgg;
 
-import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.MapColor;
+import net.minecraft.block.AbstractBlock.Settings;
+import net.minecraft.block.piston.PistonBehavior;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
 
 @Mixin(Blocks.class)
 public class MBlocksMixin {
 
-    // Getting the block id using @Inject yet somehow it worked perfectly
-    private static String block_id;
-    @Inject(method = "Lnet/minecraft/block/Blocks;register(Ljava/lang/String;Ljava/util/function/Function;Lnet/minecraft/block/AbstractBlock$Settings;)Lnet/minecraft/block/Block;",
-            at = @At("HEAD"),
-            cancellable = false)
-    private static void getBlockID(String id, Function<AbstractBlock.Settings, Block> factory, AbstractBlock.Settings settings, CallbackInfoReturnable ci){
-        block_id = id;
-    }
-
-    // Replace with the old dragon egg
-    @ModifyVariable(method = "Lnet/minecraft/block/Blocks;register(Ljava/lang/String;Ljava/util/function/Function;Lnet/minecraft/block/AbstractBlock$Settings;)Lnet/minecraft/block/Block;",
-                    at = @At("HEAD"))
-    private static Function<AbstractBlock.Settings, Block> injected(Function<AbstractBlock.Settings, Block> factory) {
-        return (block_id == "dragon_egg") ? NewDragonEgg::new : factory; 
+    @Inject(method = "Lnet/minecraft/block/Blocks;register(Ljava/lang/String;Lnet/minecraft/block/Block;)Lnet/minecraft/block/Block;",
+            at = @At("RETURN"),
+            cancellable = true)
+    private static void injected(String id, Block block, CallbackInfoReturnable<Block> cir) {
+        if (id == "dragon_egg") {
+            cir.setReturnValue((Block)Registry.register(Registries.BLOCK, id, new NewDragonEgg(Settings.create().mapColor(MapColor.BLACK).strength(3.0F, 9.0F).luminance((state) -> {
+                return 1;
+            }).nonOpaque().pistonBehavior(PistonBehavior.DESTROY))));
+        }
     }
 }
