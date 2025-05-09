be90389a2d4ee8ceec68e2c4a8ded7a0d2d83ffa
MikeStorm03 <quockhanh.09.02.03@gmail.com>
2025-05-09 12:44:15 +0700

    Add gamerule translation. Hope this mixin work well.

--- src/main/java/com/msg/mixin/MBlocksMixin.java
+++ src/main/java/com/msg/mixin/MBlocksMixin.java
@@ -4,27 +4,23 @@ import java.util.function.Function;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
 import com.msg.block.NewDragonEgg;
 
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
-import net.minecraft.registry.RegistryKey;
-import net.minecraft.registry.RegistryKeys;
-import net.minecraft.util.Identifier;
 
 @Mixin(Blocks.class)
 public class MBlocksMixin {
-
-    @Inject(method = "Lnet/minecraft/block/Blocks;register(Ljava/lang/String;Ljava/util/function/Function;Lnet/minecraft/block/AbstractBlock$Settings;)Lnet/minecraft/block/Block;",
-            at = @At("HEAD"),
-            cancellable = true)
-    private static void inject(String id, Function<AbstractBlock.Settings, Block> factory, AbstractBlock.Settings settings, CallbackInfoReturnable<Block> cir){
-        if (id == "dragon_egg") {
-            cir.setReturnValue(Blocks.register((RegistryKey<Block>)RegistryKey.of(RegistryKeys.BLOCK, Identifier.ofVanilla(id)), NewDragonEgg::new, settings));
+    @ModifyArgs(method = "<clinit>",
+                at = @At(value = "INVOKE",
+                target = "Lnet/minecraft/block/Blocks;register(Ljava/lang/String;Ljava/util/function/Function;Lnet/minecraft/block/AbstractBlock$Settings;)Lnet/minecraft/block/Block;"))
+    private static void inject(Args args) {
+        if (args.get(0) == "dragon_egg") {
+            args.set(1, (Function<AbstractBlock.Settings, Block>) NewDragonEgg::new);
         }
     }
 }
