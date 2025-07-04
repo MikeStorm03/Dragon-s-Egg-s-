019aac24e72c3012d24c8a727a857ecbdffaabdb
MikeStorm03 <quockhanh.09.02.03@gmail.com>
2025-07-04 13:01:33 +0700

    version and value and dependence

--- src/main/java/com/msg/util/Consants.java
+++ src/main/java/com/msg/util/Consants.java
@@ -3,6 +3,6 @@ package com.msg.util;
 import net.minecraft.state.property.IntProperty;
 
 public interface Consants {
-    public static int MAX_EGG = 100; // 1048575 for release
+    public static int MAX_EGG = 1048575; // 1048575 for release
     public static final IntProperty GENERATION = IntProperty.of("generation", 0, MAX_EGG);
 }
