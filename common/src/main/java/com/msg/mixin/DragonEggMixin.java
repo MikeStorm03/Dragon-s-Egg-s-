c1b5d51083b94d1686b17325568cd96368b7be53
MikeStorm03 <quockhanh.09.02.03@gmail.com>
2025-07-10 08:03:31 +0700

    fixed crash

--- common/src/main/java/com/msg/mixin/DragonEggMixin.java
+++ common/src/main/java/com/msg/mixin/DragonEggMixin.java
@@ -2,9 +2,6 @@ package com.msg.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import com.msg.DragonsEggSConstants;
 
@@ -19,11 +16,6 @@ public class DragonEggMixin extends Block{
     
     public DragonEggMixin(Properties properties) {
         super(properties);
-    }
-
-    @Inject(method = "<init>(Lnet/minecraft/world/level/block/state/BlockBehaviour/Properties;I)V", at = @At("TAIL"))
-    public void inject(Properties properties, CallbackInfo ci) {
-        registerDefaultState(defaultBlockState().setValue(DragonsEggSConstants.GENERATION, 0));
     }
 
     @Unique
